e877ed321beb60d3aabd2f98b17587b23e6508b6
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-05-22 22:13:19 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Golf::Application.config.secret_key_base = '73ff782872aaf7747ac9778ae74d3dd93ba73ef34b1b498991a8517ecd55c9f4fe04fea8a306a58a89e7ca8a28eb0ab5207bcadbfc0c783b66cbe8ce2b5217b0'
