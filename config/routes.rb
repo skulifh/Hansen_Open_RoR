a2f0c43ba91a1568f4000628136c2f1873b40d0b
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-05-23 14:56:45 +0200

    further implementation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Golf::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :blogs
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
