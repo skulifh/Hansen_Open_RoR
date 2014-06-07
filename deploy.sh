8757eebffe26c737d0fc614774579120a51cccb2
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-06-07 17:06:43 +0100

    now possible to view photos before submitting the gallery

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+sudo sh -c "echo starting"
+git pull
+bundle --deployment
+rake db:migrate RAILS_ENV='production'
+rake assets:precompile RAILS_ENV='production'
+sudo /etc/init.d/nginx restart
