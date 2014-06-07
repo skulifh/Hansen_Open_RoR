c307f1b840295050a10fed644776ebc21eabf0fb
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-06-07 15:56:59 +0100

    created edit functionality for blogs and galleries

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,14 +10,42 @@ class BlogsController < ApplicationController
 		end
 	end
 
+	def update
+		if current_user
+			blog = Blog.find(params[:id])
+			blog.update_attributes(blog_params)
+			redirect_to blogs_path
+		else
+			redirect_to blogs_path
+		end
+	end
+
+	def edit
+		if current_user
+			@blog = Blog.find(params[:id])
+		else
+			redirect_to blogs_path
+		end
+	end
+
+	def destroy
+		if current_user
+			@blog = Blog.find(params[:id])
+			@blog.destroy
+			redirect_to blogs_path
+		else
+			redirect_to blogs_path
+		end
+	end
+
 	def create
 		@blog = Blog.new(blog_params)
 		@blog.user_id = current_user.id
 		if @blog.save
-      		redirect_to root_path
-    	else
-      		redirect_to root_path
-    	end
+			redirect_to root_path
+		else
+			redirect_to root_path
+		end
 	end
 
 	def show
@@ -27,6 +55,6 @@ class BlogsController < ApplicationController
 	private
 
 	def blog_params
-  	  params.require(:blog).permit(:title, :text, :photo)
-    end
+		params.require(:blog).permit(:title, :text, :photo)
+	end
 end
--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -11,32 +11,46 @@ class GalleryController < ApplicationController
   end
 
   def update
-    
+    if current_user
+      gallery = Gallery.find(params[:id])
+      gallery.update_attributes(gallery_params)
+      redirect_to gallery_index_path
+    else
+      redirect_to gallery_index_path
+    end
+  end
+
+  def edit
+    if current_user
+      @gallery = Gallery.find(params[:id])
+    else
+      redirect_to gallery_index_path
+    end
   end
 
   def create
-		@gallery = Gallery.new(gallery_params)
+    @gallery = Gallery.new(gallery_params)
     @gallery.user_id = current_user.id
-		if @gallery.save
-      		redirect_to root_path
-    	else
-      		redirect_to root_path
-    	end
-	end
+    if @gallery.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   def destroy
     if current_user
-            @gallery = Gallery.find(params[:id])
-            @gallery.destroy
-            redirect_to gallery_index_path
-        else
-            redirect_to gallery_index_path
-        end
+      @gallery = Gallery.find(params[:id])
+      @gallery.destroy
+      redirect_to gallery_index_path
+    else
+      redirect_to gallery_index_path
+    end
   end
 
   private
 
-	def gallery_params
-  	  params.require(:gallery).permit(:name, :description, :setid, :photo)
-    end
+  def gallery_params
+   params.require(:gallery).permit(:name, :description, :setid, :photo)
+ end
 end
