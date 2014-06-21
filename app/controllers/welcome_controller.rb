d92702ca48f9609113fbae1b53729e9a4f5bc9c8
Skuli Freyr Hinriksson <skuli@sareye.com>
2014-06-21 17:08:59 +0100

    frontpage fix

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   def index
   	@blogs = Blog.find(:all, :order => "created_at desc", :limit => 2)
   	@galleries = Gallery.find(:all, :order => "created_at desc", :limit => 3)
-  	@users = User.find(:all)
+  	@users = User.all(:order => "fullname ASC")
   	@images = Image.find(:all)
   end
 end
